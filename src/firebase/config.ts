350d320ed96183dac93451bd52ca790c5bb0687f
pc mania <pcmaniasuporte2017@gmail.com>
2025-12-01 21:16:59 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-516051115-a8e0e",
+  "appId": "1:1015702983462:web:374fbb40bad7916567a7e7",
+  "apiKey": "AIzaSyASD5jGJn1T57_ZCi-I1_nKIidz9m-wO3s",
+  "authDomain": "studio-516051115-a8e0e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1015702983462"
+};
