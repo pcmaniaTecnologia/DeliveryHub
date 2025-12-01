6a8b0a9cbea13fdfb510719c493bf54698bd8b3d
pc mania <pcmaniasuporte2017@gmail.com>
2025-12-01 22:04:41 +0000

    Try fixing this error: `Runtime Error: Error: (0 , {imported module [pro

--- src/app/dashboard/coupons/page.tsx
+++ src/app/dashboard/coupons/page.tsx
@@ -3,7 +3,7 @@
 
 import { useState } from 'react';
 import { MoreHorizontal, PlusCircle } from 'lucide-react';
-import { useForm } from 'react';
+import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import { Badge } from '@/components/ui/badge';
